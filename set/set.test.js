18b19d9db6574e7d2c57ec7137e3153a1b8be2f9
2Zero1 <Good7219@hotmail.com>
2020-04-28 00:00:57 +0900

    Make Set DS by JS
    With JWM, LYH

--- set/set.test.js
+++ set/set.test.js
@@ -10,6 +10,10 @@
     - 동일
     - 부분집합
 8. 집합 연산.
+    - 교집합
+    - 합집합
+    - 곱집합
+    - 차집합
 */
 class MySet {
     constructor(...values) {
@@ -61,11 +65,112 @@ class MySet {
     }
 
     isSubsetOf(set) {
-        
+        if(this.size() == 0 && set.size() == 0) {
+            return true;
+        }
+        if(this.size()==0) {
+            return true;
+        }
+        if(this.size() > set.size()) {
+            return false;
+        }
+
+        for(let i = 0; i < this.size() && i < set.size(); i++) {
+            if (!set.include(this.values[i])) {
+                return false;
+            }
+        }
+        return true;
+
     }
+    union(otherSet) {
+        const set = new MySet(...otherSet.getValues(),...this.getValues());
+        return set;
+    }
+    intersect(otherSet) {
+        return new MySet(...this.getValues().filter(v => otherSet.include(v)));
+    } 
+    difference(otherSet) {
+        const thisSet = new MySet(...this.getValues());
+        otherSet.getValues().forEach(v => thisSet.delete(v));
+        return new MySet(...thisSet.getValues());
+    }
+
 }
 
 describe('set', () => {
+
+    test('Difference', () => {
+        const set1 = new MySet();
+        const set2 = new MySet();
+        expect(set1.difference(set2).getValues()).toEqual([]);
+        set1.add(1);
+        set1.add(2);
+        expect(set1.difference(set2).getValues()).toEqual([1,2]);
+        expect(set2.difference(set1).getValues()).toEqual([]);
+
+        set2.add(1);
+        expect(set1.difference(set2).getValues()).toEqual([2]);
+        expect(set2.difference(set1).getValues()).toEqual([]);
+        
+        set1.add(3);
+        set1.add(4);
+        set1.add(5);
+        set1.add(6);
+        expect(set1.difference(set2).getValues()).toEqual([2,3,4,5,6]);
+        expect(set2.difference(set1).getValues()).toEqual([]);
+
+
+    })
+    test('intersection', () => {
+        const set1 = new MySet();
+        const set2 = new MySet();
+        expect(set1.intersect(set2).getValues()).toEqual([]);
+        set1.add(1);
+        expect(set1.intersect(set2).getValues()).toEqual([]);
+        set2.add(1);
+        expect(set1.intersect(set2).getValues()).toEqual([1]);
+        set1.add(2);
+        expect(set1.intersect(set2).getValues()).toEqual([1]);
+        expect(set2.intersect(set1).getValues()).toEqual([1]);
+        set1.add(3);
+        set1.add(4);
+        set1.add(5);
+        set2.add(2);
+        set2.add(3);
+        set2.add(7);
+        expect(set2.intersect(set1).getValues()).toEqual([1,2,3]);
+
+
+
+
+        // set1.add(2);
+        // set1.add(3);
+        // set1.add(4);
+
+        // set2.add(1);
+        // set2.add(2);
+        // set2.add(3);
+        // expect(set1.intersect(set2).getValues()).toEqual([1,2,3]);
+
+
+    })
+    test('union', () => {
+        const set1 = new MySet();
+        const set2 = new MySet();
+        expect(set1.union(set2).getValues()).toEqual([]);
+        // set1.add(1);
+        // set1.add(2);
+        // set1.add(3);
+        // set1.add(4);
+        // expect(set1.union(set2).getValues()).toEqual([1,2,3,4]);
+        // set2.add(1);
+        // set2.add(2);
+        // set2.add(3);
+        // expect(set1.union(set2).getValues()).toEqual([1,2,3,4]);
+
+    })
+
     test('makeSet', () => {
         
         const set = new MySet(1,2,3,4,5);
@@ -130,6 +235,25 @@ describe('set', () => {
         expect(set1.isSubsetOf(set2)).toBeTruthy();
 
         set2.add(3);
-        expect(set1.isSubsetOf(set2)).toBeFalsy();
-    })
+        expect(set1.isSubsetOf(set2)).toBeTruthy();
+
+        expect(set2.isSubsetOf(set1)).toBeFalsy();
+
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+        set1.add(4);
+        set1.add(5);
+        expect(set1.isSubsetOf(set2)).toBeFalsy();  
+        expect(set2.isSubsetOf(set1)).toBeTruthy();
+
+        set2.add(1);
+        set2.add(2);
+        expect(set2.isSubsetOf(set1)).toBeTruthy();
+        set2.add(4);
+        set2.add(5);
+        expect(set2.isSubsetOf(set1)).toBeTruthy();
+        expect(set1.isSubsetOf(set2)).toBeTruthy();
+
+    });
 });
